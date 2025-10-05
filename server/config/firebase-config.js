af9f6b6a57de7baff00503fc2eaf6a73da18c67a
Saurabh Kumar <121312683+saurabh2226@users.noreply.github.com>
2025-10-05 05:41:46 +0530

    Update firebase-config.js

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,7 +1,8 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-import serviceAccountKey from "./serviceAccountKey.json" with { type: "json" };
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 
 const app = initializeApp({
   credential: cert(serviceAccountKey),
